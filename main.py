360738aaa019b1c521b66ce0b96f3ed326f00393
md-173 <durkanmick@gmail.com>
2026-04-04 13:39:27 +0800

    Command line prompting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
@@ -7,22 +8,29 @@ MODEL = "gemini-2.5-flash"
 def main():
     load_dotenv()
     
+    # Get API key
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key == None:
         raise RuntimeError("Api key not found in env file")
-    client = genai.Client(api_key=api_key)
+    client = genai.Client(api_key=api_key) 
 
-    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    # Parse command line prompt argument
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
 
+    # Generate content
     content = client.models.generate_content(
             model=MODEL,
-            contents=prompt)
-    print(f"User prompt: {prompt}")
+            contents=args.user_prompt)
+    print(f"User prompt: {args.user_prompt}")
 
+    # Get metadata
     content_metadata = content.usage_metadata
     if content_metadata == None:
         raise RuntimeError("Generated Content Metadata does not exist")
 
+    # Output token counts and response
     print(f"Prompt tokens: {content_metadata.prompt_token_count}")
     print(f"Response tokens: {content_metadata.candidates_token_count}")
     print(f"Response:\n{content.text}")
