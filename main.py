7aa7028739f092ac6cd1504469f63f118e6d6ef9
md-173 <durkanmick@gmail.com>
2026-04-04 13:55:00 +0800

    modularise content generation

--- main.py
+++ main.py
@@ -11,18 +11,18 @@ def main():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     args = parser.parse_args()
-   
-    # List of all messages
-    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-
+    
     # Get API key
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
         raise RuntimeError("Api key not found in env file")
     
-    # Generate response
-    client = genai.Client(api_key=api_key) 
+    client = genai.Client(api_key=api_key)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    generate_content(client, messages) 
+
+def generate_content(client, messages):
     response = client.models.generate_content(
             model=MODEL,
             contents=messages)
