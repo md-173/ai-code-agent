ef4ce0de93f63c0bb1d801d0628495b1976db398
md-173 <durkanmick@gmail.com>
2026-04-04 13:52:10 +0800

    Formatting and messages list

--- main.py
+++ main.py
@@ -2,39 +2,37 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 MODEL = "gemini-2.5-flash"
 
-def main():
-    load_dotenv()
-    
-    # Get API key
-    api_key = os.environ.get("GEMINI_API_KEY")
-    if api_key == None:
-        raise RuntimeError("Api key not found in env file")
-    client = genai.Client(api_key=api_key) 
-
-    # Parse command line prompt argument
+def main(): 
+    # Parse command line prompt
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     args = parser.parse_args()
+   
+    # List of all messages
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-    # Generate content
-    content = client.models.generate_content(
+    # Get API key
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("Api key not found in env file")
+    
+    # Generate response
+    client = genai.Client(api_key=api_key) 
+    response = client.models.generate_content(
             model=MODEL,
-            contents=args.user_prompt)
-    print(f"User prompt: {args.user_prompt}")
-
-    # Get metadata
-    content_metadata = content.usage_metadata
-    if content_metadata == None:
-        raise RuntimeError("Generated Content Metadata does not exist")
+            contents=messages)
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
 
     # Output token counts and response
-    print(f"Prompt tokens: {content_metadata.prompt_token_count}")
-    print(f"Response tokens: {content_metadata.candidates_token_count}")
-    print(f"Response:\n{content.text}")
-
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response:\n{response.text}")
 
 if __name__ == "__main__":
     main()
