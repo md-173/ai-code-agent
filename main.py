a38addfc65c96970b5c10bf8cc2f8f001cbf9dad
md-173 <durkanmick@gmail.com>
2026-04-07 22:11:57 +0800

    Add Agent loop

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
 import argparse
+import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
 from functions.get_files_info import schema_get_files_info
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 MODEL = "gemini-2.5-flash"
 
@@ -27,8 +28,17 @@ def main():
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     if args.verbose:
         print(f"User prompt: {args.user_prompt}\n")
-    
-    generate_content(client, messages, args.verbose) 
+   
+    for _ in range(20):
+        final = generate_content(client, messages, args.verbose)
+        if final:
+            print(final)
+            break
+    else:
+        print("Max iterations reached without a final response")
+        sys.exit(1)
+
+
 
 def generate_content(client, messages, verbose):
     response = client.models.generate_content(
@@ -44,13 +54,34 @@ def generate_content(client, messages, verbose):
     if verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+    if response.candidates is None:
+        raise Exception("Response has no candidates")
+    else:
+        for candidate in response.candidates:
+            messages.append(candidate.content)
+
     function_calls = response.function_calls
     
+    function_result_list = []
     if function_calls is not None:
         for call in function_calls:
-            print(f"Calling function: {call.name}({call.args})")
+            function_call_result = call_function(call, verbose)
+            if not function_call_result.parts:
+                raise Exception(f'Function call {call.name} has no parts list')
+            if function_call_result.parts[0].function_response is None:
+                raise Exception(f'Function response of {call.name} is empty')
+            if function_call_result.parts[0].function_response.response is None:
+                raise Exception(f'Function result of {call.name} is none')
+            function_result_list.append(function_call_result.parts[0])
+            if verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+
     else:
-        print(f"Response:\n{response.text}")
+        return f"Response:\n{response.text}"
+        
+
+    messages.append(types.Content(role="user", parts=function_result_list))
 
 if __name__ == "__main__":
     main()
